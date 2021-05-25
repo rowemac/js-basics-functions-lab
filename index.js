e75376a332debac62e9ef120f3f736028a954788
Mac Rowe <m.rowe42@gmail.com>
2021-05-25 13:26:20 -0400

    commit tree 14aabd5986e2a48d6db9a2cee53fe5ea9edb49db

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42) {
+        return blocks - 42
+    }
+    else if (blocks <= 42) {
+        return 42 - blocks
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264)
+}
+
+function distanceTravelledInFeet(beginning, end) {
+    if (beginning >= end) {
+        return (beginning - end) * 264
+    } 
+    else {
+        return (end - beginning) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let cost; 
+
+    switch(true) {
+        case (distance <= 400):
+            cost = 0
+        break;
+
+        case (distance > 400 && distance <= 2000):
+            cost = ((distance - 400) * 0.02)
+        break;
+
+        case (distance > 2000 && distance <= 2500):
+            cost = 25
+        break;
+
+        default:
+            return "cannot travel that far"
+    };
+    
+    return cost;
+
+}
